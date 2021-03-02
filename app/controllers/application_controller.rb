fa86b2507c72fb4a64be53c598975330dbf941e9
mengshalitsofi <mengshalitsofi@gmail.com>
2021-03-01 18:20:03 -0800

    commit tree a74bc6527220cbb04fd8d60f17783af6c916589b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,39 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  post '/articles' do
+    article = Article.create(:title => params[:title], :content => params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.where(:id => params[:id])[0]
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.where(:id => params[:id])[0]
+    article.update(:title => params[:title], :content => params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.where(:id => params[:id])[0]
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    article = Article.where(:id => params[:id])[0]
+    article.delete
+    redirect '/articles'
   end
 end
